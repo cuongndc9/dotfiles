5d7bfa0e742d1d6434eabda0713e76b752add7c4
Cuong Nguyen <cuongndc9@gmail.com>
2025-02-24 17:28:00 +0000

    👋 goodbye 🤡 coding

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+💻 dotfiles and 🦄 more (MacOS).
+
+```sh
+yes | /bin/bash -c "$(curl -sSL https://raw.githubusercontent.com/cuongndc9/dotfiles/main/installation.sh)"
+```
---
+++ installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install software
+brew install --cask notion
+brew install --cask chatgpt
+brew install --cask spotify
